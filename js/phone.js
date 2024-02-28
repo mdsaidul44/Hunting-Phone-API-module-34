2b5cfd3c9a41b006c97ec7241367f9c67eae927a
mdsaidul44 <islam88758@gmail.com>
2024-02-28 12:36:03 +0600

    phone section added in javascript

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,46 @@
-console.log('hello hunting')
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    const data = await res.json()
+    const phones = data.data;
+    // console.log(phones)
+    displayPhones(phones)
+}
+
+
+const displayPhones = phones => {
+    phones.forEach(phone => {
+
+        const phoneContainer = document.getElementById('phone-container')
+
+        // console.log(phone)
+        // 2 create a div
+        const phoneCard = document.createElement('div')
+        phoneCard.classList = `card bg-gray-100 p-5 shadow-xl`
+        // 3. set innerHTML
+        phoneCard.innerHTML= `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `
+        // 4. appendChild 
+        phoneContainer.appendChild(phoneCard)
+ 
+    }) 
+}
+
+
+
+// handle search button.
+const handleSearch = () =>{
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value;
+    console.log(searchText)
+    loadPhone(searchText)
+}
+
+// loadPhone()
